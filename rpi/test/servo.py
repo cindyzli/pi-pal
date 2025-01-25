38ebbf93001ce12cbf7acdf23cb190971299ba8e
ElemelonWind <c.yang.2023@gmail.com>
2025-01-25 13:39:07 -0500

    servo code

--- rpi/test/servo.py
+++ rpi/test/servo.py
@@ -3,10 +3,9 @@ from time import sleep
 
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
-while True:
-    servo.angle = 90
-    sleep(2)
-    servo.angle = 0
-    sleep(2)
-    servo.angle = -90
-    sleep(2)
+servo.angle = 90
+sleep(1)
+servo.angle = 0
+sleep(0.2)
+servo.angle = 90
+sleep(1)
