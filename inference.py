f5e43d2cf79ff1cad45013f60f07d82edbc0bcb7
elise-yz <elise.zyh@gmail.com>
2025-01-25 11:10:27 -0500

    cv changes

--- inference.py
+++ inference.py
@@ -7,6 +7,9 @@ detector = HandDetector(staticMode=False, maxHands=1, modelComplexity=1, detecti
 # Default laptop webcam index
 video = cv2.VideoCapture(0) 
 
+# Number of fingers up
+fingers = 0
+
 while True: 
     _, img = video.read() # Capture image frame
     
@@ -19,17 +22,25 @@ while True:
             
             # Getting number of fingers up
             fingerup = detector.fingersUp(lmlist)   
-              
-            if fingerup == [0, 1, 0, 0, 0]: 
-                print("one")
-            if fingerup == [0, 1, 1, 0, 0]: 
-                print("two")
-            if fingerup == [0, 1, 1, 1, 0]: 
-                print("three")
-            if fingerup == [0, 1, 1, 1, 1]: 
-                print("four")
-            if fingerup == [1, 1, 1, 1, 1]: 
-                print("five")
+            
+            if fingerup == [0, 0, 0, 0, 0] and fingers != 0:
+                fingers = 0
+                print(fingers)
+            if fingerup == [0, 1, 0, 0, 0] and fingers != 1: 
+                fingers = 1
+                print(fingers)
+            if fingerup == [0, 1, 1, 0, 0] and fingers != 2: 
+                fingers = 2
+                print(fingers)
+            if fingerup == [0, 1, 1, 1, 0] and fingers != 3: 
+                fingers = 3
+                print(fingers)
+            if fingerup == [0, 1, 1, 1, 1] and fingers != 4: 
+                fingers = 4
+                print(fingers)
+            if fingerup == [1, 1, 1, 1, 1] and fingers != 5: 
+                fingers = 5
+                print(fingers)
 
     cv2.imshow("Video", img) 
     
